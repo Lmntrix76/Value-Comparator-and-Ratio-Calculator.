ee8d080338171db0c3019d89d2fbc71185f36e71
Lmntrix76 <147538065+Lmntrix76@users.noreply.github.com>
2024-08-17 21:13:14 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnFindDecimal = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnFindDecimal
+        '
+        Me.btnFindDecimal.Location = New System.Drawing.Point(105, 37)
+        Me.btnFindDecimal.Name = "btnFindDecimal"
+        Me.btnFindDecimal.Size = New System.Drawing.Size(75, 47)
+        Me.btnFindDecimal.TabIndex = 0
+        Me.btnFindDecimal.Text = "Find the Decimal Value"
+        Me.btnFindDecimal.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(105, 100)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 1
+        Me.btnClose.Text = "Exit Program"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(281, 175)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnFindDecimal)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnFindDecimal As Button
+    Friend WithEvents btnClose As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Dim intValue1 As Integer = 0
+    Dim intValue2 As Integer = 0
+    Dim decAnswer As Decimal = 0
+    Dim strValue1 As String = ""
+    Dim strValue2 As String = ""
+    Dim intValid As Integer = 0
+    Public Property Value1 As String
+    Public Property Value2 As String
+
+    Private Sub btnFindDecimal_Click(sender As Object, e As EventArgs) Handles btnFindDecimal.Click
+        strValue1 = InputBox(" Please enter Value 1 ", " Value 1 ", 0)
+        If MessageBox.Show(" The value entered was " & strValue1, " Validate Value 1 ", MessageBoxButtons.OKCancel) =
+                Windows.Forms.DialogResult.Cancel Then
+            MessageBox.Show(" Value 1 was invalid ")
+            Exit Sub
+        End If
+
+        strValue1 = InputBox(" Please enter Value 2 ", " Value 2 ", 0)
+        If MessageBox.Show(" The vaue entered was " & strValue2, " Validate Value 2 ", MessageBoxButtons.OKCancel) =
+                Windows.Forms.DialogResult.Cancel Then
+            MessageBox.Show(" Value 2 was invalid ")
+            Exit Sub
+        End If
+
+        Integer.TryParse(Value1, intValue1)
+        Integer.TryParse(Value2, intValue2)
+        If intValue2 > intValue1 Then
+            MessageBox.Show(" Value 2: " & intValue2 & " was the highest ")
+            decAnswer = intValue1 / intValue2
+        Else
+            MessageBox.Show(" Value 1: " & intValue1 & " was the highest ")
+            decAnswer = intValue2 / intValue1
+        End If
+        MessageBox.Show(" The Decimal value = " &
+            Math.Round(decAnswer, 2))
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        'Close the program
+        Me.Close()
+    End Sub
+End Class
